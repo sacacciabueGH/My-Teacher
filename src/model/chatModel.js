e3c3d485c41760634e7728e4e84c6eeddef75ef8
sacacciabueGH <santiagocacciabue@hotmail.com>
2024-11-06 20:32:42 -0300

    bd error

--- src/model/chatModel.js
+++ src/model/chatModel.js
@@ -1,4 +1,4 @@
-import db from '../config/db.js';
+import db from '../DB/db.js';
 
 export const guardarMensaje = (alumno_id, profesor_id, mensaje) => {
     return new Promise((resolve, reject) => {
